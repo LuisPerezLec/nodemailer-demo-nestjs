1ee565ba2040e6cb609d53c922392238f56f74a1
LuisPerezLec <93443241+LuisPerezLec@users.noreply.github.com>
2024-12-30 11:42:35 -0600

    :construction: Utilizando imagenes como cid

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -13,8 +13,54 @@ export class MailService {
         from: 'pruebas.informatica@upa.edu.mx', // sender address
         subject: 'Testing Nest MailerModule ✔', // Subject line
         template: 'index',
+        attachments: [
+          {
+            filename: 'image-1.png',
+            path: join(__dirname, '..', '..', 'src', 'templates', 'images', 'image-1.png'),
+            cid: 'unique1@nodemailer.com',
+          }, // image-2.png
+          {
+            filename: 'image-2.png',
+            path: join(__dirname, '..', '..', 'src', 'templates', 'images', 'image-2.jpeg'),
+            cid: 'unique2@nodemailer.com',
+          },
+          {
+            filename: 'image-3.png',
+            path: join(__dirname, '..', '..', 'src', 'templates', 'images', 'image-3.png'),
+            cid: 'unique3@nodemailer.com',
+          },
+          {
+            filename: 'image-4.png',
+            path: join(__dirname, '..', '..', 'src', 'templates', 'images', 'image-4.png'),
+            cid: 'unique4@nodemailer.com',
+          },
+          {
+            filename: 'image-5.png',
+            path: join(__dirname, '..', '..', 'src', 'templates', 'images', 'image-5.png'),
+            cid: 'unique5@nodemailer.com',
+          },
+          {
+            filename: 'image-6.png',
+            path: join(__dirname, '..', '..', 'src', 'templates', 'images', 'image-6.png'),
+            cid: 'unique6@nodemailer.com',
+          },
+          {
+            filename: 'image-7.png',
+            path: join(__dirname, '..', '..', 'src', 'templates', 'images', 'image-7.png'),
+            cid: 'unique7@nodemailer.com',
+          },
+          {
+            filename: 'image-8.png',
+            path: join(__dirname, '..', '..', 'src', 'templates', 'images', 'image-8.png'),
+            cid: 'unique8@nodemailer.com',
+          },
+        ],
       })
-      .then()
-      .catch(() => {});
+      .then(messageInfo => {
+        console.log(messageInfo);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
   }
 }
